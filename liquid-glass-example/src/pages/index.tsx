fc3f54de9cba7bc71ff5e06d71065f53fd14cb24
KrazyManJ <KrazyManJ@gmail.com>
2025-12-16 06:43:59 +0100

    Fixed font tailwind class in index

--- liquid-glass-example/src/pages/index.tsx
+++ liquid-glass-example/src/pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home() {
 
   return (
     <div
-      className={`${geistSans.className} grid grid-cols-1 grid-rows-2 md:grid-rows-1 md:grid-cols-3 shadow-2xl w-full max-w-5xl mx-auto md:my-10 h-screen md:max-h-[calc(100vh-5rem)] md:rounded-3xl overflow-hidden font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.className} grid grid-cols-1 grid-rows-2 md:grid-rows-1 md:grid-cols-3 shadow-2xl w-full max-w-5xl mx-auto md:my-10 h-screen md:max-h-[calc(100vh-5rem)] md:rounded-3xl overflow-hidden font-(family-name:--font-geist-sans)`}
     >
       {/* Left Panel - Glass Effect Demo */}
       <div className="flex-1 relative overflow-auto min-h-screen md:col-span-2" ref={containerRef} onScroll={handleScroll}>
